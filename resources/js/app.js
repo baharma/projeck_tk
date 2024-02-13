c4bd3774783b2604ada0422f399ef56371c77cef
baharma <bagusaditya1899@gmail.com>
2024-02-13 13:34:39 +0800

    change app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,13 +11,7 @@ import { $ , jQuery } from "../theme/vendor/libs/jquery/jquery.js";
 window.$ = $;
 window.jQuery = jQuery;
 
-import "../theme/vendor/libs/popper/popper.js";
-import "../theme/js/bootstrap.js";
-import "../theme/vendor/libs/perfect-scrollbar/perfect-scrollbar.js";
-import "../theme/js/menu.js";
-import "../theme/vendor/libs/apex-charts/apexcharts.js";
-import "../theme/vendor/libs/apex-charts/apexcharts.js";
-import "../theme/js/dashboards-analytics.js";
+
 
 import Alpine from 'alpinejs';
 
